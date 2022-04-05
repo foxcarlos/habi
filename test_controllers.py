69df4a82cb3164bb89b2b5d97afee4ab8add88b8
foxcarlos <foxcarlos@gmail.comm>
2022-04-05 13:51:14 -0300

    [Fix/habi] New Test controllers and model

--- test_controllers.py
+++ test_controllers.py
@@ -2,6 +2,7 @@
 import unittest
 
 from controllers.inmuebles import ControllersInmueble
+from models.inmuebles import ModelsInmueble, DBApi
 
 
 class TestControllers(unittest.TestCase):
@@ -9,6 +10,11 @@ class TestControllers(unittest.TestCase):
         pnf = ControllersInmueble({}, '')
         result = pnf._not_found('')
         self.assertEqual(result, {"message": "PAGE NOT FOUND"})
+
+    def test_search_inmueble_name(self):
+        body_json_recived = {"status_search": ["vendido"]}
+        sbs = ControllersInmueble._search_by_status_name(self, body_json_recived)
+        self.assertTrue(sbs)
 
     def test_search_inmueble(self):
         body_json_recived = {"status_search": [3, 4, 5]}
@@ -26,6 +32,65 @@ class TestControllers(unittest.TestCase):
         response = ControllersInmueble(body_json_recived, '/filter/')
         sbs = response._filter_by(body_json_recived)
         self.assertTrue(sbs)
+
+
+class TestModelsInmuebleDBApi(unittest.TestCase):
+    def test_connect_ok(self):
+        db = DBApi()
+        connect = db.connect()
+        self.assertTrue(connect)
+
+    def test_connect_fail(self):
+        db = DBApi()
+        db.host = '0.0.0.0'
+        connect = db.connect()
+        self.assertFalse(connect)
+
+    def test_query_ok(self):
+        db = DBApi()
+        db.connect()
+        result = db.exec_query("SELECT CURDATE()")
+        self.assertTrue(result)
+
+
+class TestModelsInmueble(unittest.TestCase):
+    def test_parse_search_data_empty(self):
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.parse_search_data("")
+        self.assertEqual(result, [])
+
+    def test_parse_search_data_ok(self):
+        data = [(3, 1, 'calle 23 #45-67', 'bogota', 120000000,
+                'Hermoso apartamento en el centro de la ciudad', 3, 'pre_venta')]
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.parse_search_data(data)
+        self.assertTrue(result)
+
+    def test_search_by_satus_name_ok(self):
+        json = {"status_search": ["pre_venta", "vendido"]}
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.search_by_status_name(json)
+        self.assertTrue(result)
+
+    def test_search_by_satus_name_not_found(self):
+        json = {"status_search": ["otro"]}
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.search_by_status_name(json)
+        self.assertEqual(result, {'inmuebles por status': []})
+
+    def test_filter_by_ok(self):
+        json = {"year": "2000", "city": "bogota", "status_name": "pre_venta"}
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.filter_by(json)
+        length = len(result.get('inmuebles por Anio, ciudad, status'))
+        self.assertGreater(length, 0)
+
+    def test_filter_by_not_found(self):
+        json = {"year": "3000"}
+        model_inmueble = ModelsInmueble()
+        result = model_inmueble.filter_by(json)
+        length = len(result.get('inmuebles por Anio, ciudad, status'))
+        self.assertEqual(length, 0)
 
 
 if __name__ == "__main__":
